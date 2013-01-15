d6ce4fa516edbf4e12a93a9aa165f330bba1e24e
sociocast <albert.azout@sociocast.com>
2013-01-15 13:01:42 -0500

    Update sociocast-ruby-example.rb

--- sociocast-ruby-example.rb
+++ sociocast-ruby-example.rb
@@ -12,7 +12,7 @@ require 'erb'
 # apikey - unique API key assigned to the client
 # apisecret - shared secret assigned to the client
 # timestamp - number of seconds since the Unix Epoch at which the request was made
-# parameters - the Hash containing parameters to the API call, may be empty but not nil
+# parameters - the Hash containing parameters to the API call
 def calculate_hash(apikey, apisecret, timestamp, parameters)
         # gather values included in hash, sort them alphabetically
 	sig_params = Hash.new
@@ -25,7 +25,7 @@ def calculate_hash(apikey, apisecret, timestamp, parameters)
 	sig_params.sort.map do |key,value|
 	    string_to_hash = string_to_hash + key.to_s + "=" + value.to_s + "&"
 	end
-	string_to_hash = string_to_hash.chomp('&') # Ensuring that the last '&' is removed. Artifact introduced by this example code.
+	string_to_hash = string_to_hash.chomp('&') # Ensuring that the last '&' is removed.
 	
 	# compute the SHA256 hash and return as a string
 	Digest::SHA256.hexdigest(string_to_hash).to_s
@@ -55,8 +55,10 @@ def get_profile_for_url(apikey, apisecret, url)
 	sig_string = 'sig=' + sig
 	
 	puts "Trying to build GET request string for URL: #{url}"
-  # Encode the URL for safe passage and ensure that the calculated signature is appended to the call along with the time stamp.
-	request_path = '/content_profile?url=' + ERB::Util.url_encode(url) + '&' + apikey_string + '&' + ts_string + '&' + sig_string 
+        # Encode the URL for safe passage and ensure that the calculated signature is appended to the call
+        # along with the time stamp.
+	request_path = '/content/profile?url=' + 
+	               ERB::Util.url_encode(url) + '&' + apikey_string + '&' + ts_string + '&' + sig_string 
 	
 	# Send the request
 	headers = { 'Content-Type' => 'application/json; charset=utf-8' }
@@ -68,11 +70,10 @@ end
 
 
 # Main code
-@server = 'api-test.scastnet.com'
+@server = 'api-test.sociocast.com'
 @port   = 80
 url     = ARGV[0] # The URL that you need to get the content profile for. 
-apikey = #YOUR_API_KEY
-
+apikey  = #YOUR_API_KEY
 secret  = #YOUR_SECRET
 
 get_profile_for_url(apikey,secret,url)
